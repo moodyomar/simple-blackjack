68adaf3bbd7e53c0f5aa5ab8c4f3f99e96de278e
Mohammad Omar Shihab <lonak.secret@gmail.com>
2022-10-25 11:48:23 +0300

    init frontend

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css'
+import {useState,useEffect} from 'react'
+
+const App = () => { 
+
+useEffect(() => {
+
+  
+
+},[] )
+
+return(
+
+<div className='App'>
+<h1>App</h1>
+</div>
+
+)
+}
+
+export default App
